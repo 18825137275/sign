774ded0d31a21a71032c94802880237702b5aab2
“18825137275” <734865521@qq.com>
2017-05-19 14:58:36 +0800

    基本修改

--- modules/database.php
+++ modules/database.php
@@ -18,7 +18,7 @@ return [
     // 服务器地址
     'hostname'    => '127.0.0.1',
     // 数据库名
-    'database'    => 'sp',
+    'database'    => 'signuppay',
     // 数据库用户名
     'username'    => 'root',
     // 数据库密码
